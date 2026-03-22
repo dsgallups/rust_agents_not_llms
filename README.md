0ebe955fe2efae97759ff62c66d3319384cac052
Daniel Gallups <dsgallups@protonmail.com>
2026-03-22 15:44:54 -0400

    add prelude

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Steps to get here
 `cargo new rust_agents_not_llms`
 # Add dependencies
 - `tokio`: an async runtime. Basically the de-facto runtime for multithreaded async executors (the full feature gives me access to all the things tokio can do. You can constrain this to be like i/o, a single-threaded executor if needed, etc.)
+- `anyhow`: Makes errors easy :) (rust has `Result<T,E>`, not `try catch`, which means you usually have to handle your errors. For this example, I'm ignoring proper error checking)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
+#![allow(
+    unused,
+    reason = "I'm not interested in IDE lints that say something is unused since this is an example"
+)]
+
+/// This is just a set of common exports that I use throughout this project.
+/// It's just useful to have
+mod prelude;
+
+/// This is a macro that spawns a tokio runtime so we can make async threads.
+///
+/// Async is pretty useful for I/O because when we actually interface with something,
+/// this system can do more work
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
 }
